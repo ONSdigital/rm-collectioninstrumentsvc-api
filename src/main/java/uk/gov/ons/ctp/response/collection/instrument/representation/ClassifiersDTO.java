0d88f784c782f9af39cdda8f380d6d42e5eb1a0c
Tejas Patel <tejas.patel@ons.gov.uk>
2018-02-27 15:35:53 +0000

    add form_type and eq_id to classifiers

--- src/main/java/uk/gov/ons/ctp/response/collection/instrument/representation/ClassifiersDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/instrument/representation/ClassifiersDTO.java
@@ -1,6 +1,7 @@
 package uk.gov.ons.ctp.response.collection.instrument.representation;
 
-import java.util.UUID;
+import java.util.Date;
+import java.util.List;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
@@ -13,9 +14,9 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
-public class CollectionInstrumentDTO {
-  private UUID id;
-  private String surveyId;
-  private ClassifiersDTO classifiers;
-
+public class ClassifiersDTO {
+    private List<String> COLLECTION_EXERCISE;
+    private List<String> RU_REF;
+    private String eq_id;
+    private String form_type;
 }

7c0fe714e0a93d88e8f22930536a97dc30c52628
Ben Jefferies <benjefferies@echosoft.uk>
2018-06-26 15:24:40 +0100

    Add formatter and checkstyle
    Adding maven plugin to format the java code automatically on builds. The
    style of the formatter is opinionated favouring googles formatting. The
    benefit of this is it allows us to easily plugin in googles check style
    file.

--- src/main/java/uk/gov/ons/ctp/response/collection/instrument/representation/CollectionInstrumentDTO.java
+++ src/main/java/uk/gov/ons/ctp/response/collection/instrument/representation/CollectionInstrumentDTO.java
@@ -1,15 +1,12 @@
 package uk.gov.ons.ctp.response.collection.instrument.representation;
 
 import java.util.UUID;
-
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * Domain model object
- */
+/** Domain model object */
 @Data
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
@@ -17,5 +14,4 @@ public class CollectionInstrumentDTO {
   private UUID id;
   private String surveyId;
   private ClassifiersDTO classifiers;
-
 }
